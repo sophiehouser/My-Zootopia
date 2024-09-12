d5f5214e854138d602a8fd37fea7bdacd8fda38f
Sophie Houser <sophiehouser@gmail.com>
2024-09-12 12:47:50 +0200

    reformat the code

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,41 +2,63 @@ import json
 
 
 def load_data(file_path):
-    """ Loads a JSON file """
+    """
+    Loads a JSON file
+    @rtype: object
+    """
     with open(file_path, "r") as handle:
         return json.load(handle)
 
 
-def get_animal_info(data):
-    """ Prints all animals """
+def serialize_animal(animal_obj):
+    """
+    Serialize an animal object to HTML
+    @param animal_obj: Dict
+    @return: String
+    """
+    output = '<li class="cards__item">'
+
+    if 'name' in animal_obj and animal_obj['name']:
+        output += f"<div class=\"card__title\">{animal_obj['name']}</div>"
+
+    output += "<p class=\"card__text\">"
+    if 'characteristics' in animal_obj:
+        if 'diet' in animal_obj['characteristics'] and animal_obj['characteristics']['diet']:
+            output += f"<strong>Diet:</strong> {animal_obj['characteristics']['diet']}<br/>"
+        if 'type' in animal_obj['characteristics'] and animal_obj['characteristics']['type']:
+            output += f"<strong>Type:</strong> {animal_obj['characteristics']['type']}<br/>"
+
+    if 'locations' in animal_obj and animal_obj['locations']:
+        output += f"<strong>Location:</strong> {animal_obj['locations'][0]}<br/>"
+
+    output += '</li>'
+
+    return output
+
+
+def serialize_animals(data):
+    """ Prints all animals
+    @param data: Dict
+    @return: String
+    """
     output = ''
-    for animal_data in data:
-        output += '<li class="cards__item">'
-
-        if 'name' in animal_data and animal_data['name']:
-            output += f"<div class=\"card__title\">{animal_data['name']}</div>"
-
-        output += "<p class=\"card__text\">"
-        if 'characteristics' in animal_data:
-            if 'diet' in animal_data['characteristics'] and animal_data['characteristics']['diet']:
-                output += f"<strong>Diet:</strong> {animal_data['characteristics']['diet']}<br/>"
-            if 'type' in animal_data['characteristics'] and animal_data['characteristics']['type']:
-                output += f"<strong>Type:</strong> {animal_data['characteristics']['type']}<br/>"
-
-        if 'locations' in animal_data and animal_data['locations']:
-            output += f"<strong>Location:</strong> {animal_data['locations'][0]}<br/>"
-
-        output += '</li>'
+    for animal_obj in data:
+        animal_html = serialize_animal(animal_obj)
+        output += animal_html
 
     return output
 
 
 def generate_animals_html(template_path, data):
-    """ Generates HTML content by replacing placeholder with animal info """
+    """ Generates HTML content by replacing placeholder with animal info
+    @param template_path: String
+    @param data: Dict
+    @return: String
+    """
     with open(template_path, 'r') as template_file:
         template_content = template_file.read()
 
-    animal_info = get_animal_info(data)
+    animal_info = serialize_animals(data)
 
     generated_html = template_content.replace('__REPLACE_ANIMALS_INFO__', animal_info)
 
