19bbdf6a637e9e5b5688c594274aa3fcca0f45ea
Sophie Houser <sophiehouser@gmail.com>
2024-09-12 12:03:01 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,27 +6,44 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-def print_animals(data):
+
+def get_animal_info(data):
     """ Prints all animals """
+    output = ''
     for animal_data in data:
         if 'name' in animal_data and animal_data['name']:
-            print(f"Name: {animal_data['name']}")
+            output += f"Name: {animal_data['name']}\n"
 
         if 'characteristics' in animal_data:
             if 'diet' in animal_data['characteristics'] and animal_data['characteristics']['diet']:
-                print(f"Diet: {animal_data['characteristics']['diet']}")
+                output += f"Diet: {animal_data['characteristics']['diet']}\n"
             if 'type' in animal_data['characteristics'] and animal_data['characteristics']['type']:
-                print(f"Type: {animal_data['characteristics']['type']}")
+                output += f"Type: {animal_data['characteristics']['type']}\n"
 
         if 'locations' in animal_data and animal_data['locations']:
-            print(f"Location: {animal_data['locations'][0]}")
+            output += f"Location: {animal_data['locations'][0]}\n"
 
-        print()
+    return output
+
+
+def generate_animals_html(template_path, data):
+    """ Generates HTML content by replacing placeholder with animal info """
+    with open(template_path, 'r') as template_file:
+        template_content = template_file.read()
+
+    animal_info = get_animal_info(data)
+
+    generated_html = template_content.replace('__REPLACE_ANIMALS_INFO__', animal_info)
+
+    return generated_html
 
 
 def main():
     data = load_data('animals_data.json')
-    print_animals(data)
+    new_html = generate_animals_html("animals_template.html", data)
+
+    with open('animals.html', 'w') as output_file:
+        output_file.write(new_html)
 
 
 if __name__ == '__main__':
